f7ad5c225752bb607fc217377842c80dcbdc08bb
Oleksii <lxvslnk@gmail.com>
2026-04-30 12:12:13 -0300

    chore: bump orch8-server to 0.3.1

--- Formula/orch8-server.rb
+++ Formula/orch8-server.rb
@@ -1,28 +1,28 @@
 class Orch8Server < Formula
   desc "Durable workflow orchestration engine"
   homepage "https://orch8.io"
-  version "0.2.1"
+  version "0.3.1"
   license "BUSL-1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/orch8-io/engine/releases/download/v#{version}/orch8-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "ed7f152cf8f1cb9e9e831234503c8c47500230c910b8a1fadc525602998f575f"
+      sha256 "e3422b002450b01a4d085eb952098053c5adf0e9b8f3803b4c95f9b2af6109e6"
     end
     on_intel do
       url "https://github.com/orch8-io/engine/releases/download/v#{version}/orch8-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "fd9d3aaa46435b3e3c822ecb3884807cb3e16284e67abdeeb8935d7cc62dd225"
+      sha256 "15e9470ab7457b723ddf2613f5c9e4ab0c2f5e9eaf5b517d9118e79bdcdfb711"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/orch8-io/engine/releases/download/v#{version}/orch8-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "236a13471509aab4f77f2fa7f78f210f3264b47e25f2b0ff9d28fcfc5e069f96"
+      sha256 "94d3511575203c69e11f18c614e41edeaf724c4fcfed1f2a5a7a9509c3cad5c3"
     end
     on_intel do
       url "https://github.com/orch8-io/engine/releases/download/v#{version}/orch8-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "72e3748e0308a8f4659bc549d32cea391e3fde628780fb54b3a682ca783d54d3"
+      sha256 "898fabc7876a9d8c8186c962873e3a5a3d57aefd5076d40bc82260641beb5b26"
     end
   end
 
